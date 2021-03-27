35878a0a7e5b6e2de6ce2904046dac3fc780bd35
Bruno Roncaglioni <brro@bluemail.ch>
2021-03-27 15:23:51 +0100

    Initial Commit

---
+++ RoUI/RoUI.h
@@ -0,0 +1,18 @@
+//
+//  RoUI.h
+//  RoUI
+//
+//  Created by seco on 27.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RoUI.
+FOUNDATION_EXPORT double RoUIVersionNumber;
+
+//! Project version string for RoUI.
+FOUNDATION_EXPORT const unsigned char RoUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoUI/PublicHeader.h>
+
+
